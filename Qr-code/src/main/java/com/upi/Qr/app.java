991a5428ad5df94c65bb5c230e0624e57a5646da
HPRASHNNA <49604080+HPRASHNNA@users.noreply.github.com>
2022-12-30 11:23:12 +0530

    Update app.java

--- Qr-code/src/main/java/com/upi/Qr/app.java
+++ Qr-code/src/main/java/com/upi/Qr/app.java
@@ -19,46 +19,40 @@ public class app {
 
 	public static void generateQRcode(String data, String path, String charset, Map map, int h, int w)
 			throws WriterException, IOException {
-		BitMatrix matrix = new MultiFormatWriter().encode(new String(data.getBytes(charset), charset),BarcodeFormat.QR_CODE, w, h);
-		MatrixToImageWriter.writeToFile(matrix, path.substring(path.lastIndexOf('.') + 1), new File(path));  
-		
+		BitMatrix matrix = new MultiFormatWriter().encode(new String(data.getBytes(charset), charset),
+				BarcodeFormat.QR_CODE, w, h);
+		MatrixToImageWriter.writeToFile(matrix, path.substring(path.lastIndexOf('.') + 1), new File(path));
+
 	}
 
 	public static void main(String args[]) throws WriterException, IOException, NotFoundException {
-		
-		 Scanner scanner = new Scanner(System.in);
 
+		Scanner scanner = new Scanner(System.in);
 
-		 
-	        String payeeUPIID = "hariprashnna@okaxis";
-	        
+		String payeeUPIID = "hariprashnna@okaxis";
 
-	      
-	        String payeeName = "hari";
-	        
-	        System.out.print("Enter payment amount: ");
-	        
-	        String paymentAmount = scanner.nextLine();
-	        
+		String payeeName = "hari";
 
-	        
-	        String paymentCurrency = "INR";
+		System.out.print("Enter payment amount: ");
 
-	        
-	        String paymentDescription = "fee";
-	        
-	        
-		String upi = "upi://pay?pa=" + payeeUPIID + "&pn=" + payeeName + "&am=" + paymentAmount + "&cu=" + paymentCurrency + "&tn=" + paymentDescription;
-		
+		String paymentAmount = scanner.nextLine();
+
+		String paymentCurrency = "INR";
+
+		String paymentDescription = "fee";
+
+		String upi = "upi://pay?pa=" + payeeUPIID + "&pn=" + payeeName + "&am=" + paymentAmount + "&cu="
+				+ paymentCurrency + "&tn=" + paymentDescription;
+
 		String path = "C:\\Hari\\Project\\QRDemo\\Quote.png";
-		
+
 		String charset = "UTF-8";
 		Map<EncodeHintType, ErrorCorrectionLevel> hashMap = new HashMap<EncodeHintType, ErrorCorrectionLevel>();
-	
+
 		hashMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
-		
+
 		generateQRcode(upi, path, charset, hashMap, 200, 200);
-		
+
 		System.out.println("QR Code created successfully.");
 	}
 }
